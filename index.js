978ee24e0611087634b483fdb2064b6b5c9fca83
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-05 20:45:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { match } = require("sinon");
+
 // Code your solution here
+function findMatching(drivers, string) {
+    const matchingDrivers = drivers.filter(function(driver) {
+        return driver.toUpperCase() === string.toUpperCase();
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+    const endIndex = string.length;
+    const matchingDrivers = drivers.filter(function(driver) {
+        console.log(driver.slice(0, endIndex));
+        return driver.slice(0, endIndex) === string;
+    });
+    return matchingDrivers;
+}
+
+function matchName(drivers, string) {
+    const matchingDrivers = drivers.filter((driver) => {
+        return driver.name === string;
+    })
+    return matchingDrivers;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
